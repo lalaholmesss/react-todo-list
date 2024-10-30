61bdd6b792819928b2f7c86cd51ae2be9f8f22ff
lalaholmesss <lalaholmesart00@gmail.com>
2024-10-30 17:35:38 +0400

    Initial commit

---
+++ components/TodoCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function TodoCard(props) {
+    const {children, handleDeleteTodo, index, handleEditTodo} = props
+    
+
+  return (
+    <li className='todoItem'>
+        {children}
+        <div className='actionsContainer'>
+            <button onClick={() => {
+              handleEditTodo(index)
+            }}><i className="fa-solid fa-wand-magic-sparkles"></i></button>
+            <button onClick={() => {
+              handleDeleteTodo(index)
+            }}><i className="fa-solid fa-minus"></i></button>
+        </div>
+    </li>
+  )
+}
---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+import { useState, useEffect } from "react"
+import TodoInput from "../components/TodoInput"
+import TodoList from "../components/TodoList"
+
+function App() {
+  const [todos, setTodos] = useState([""])
+  const [todoValue, setTodoValue] = useState([""])
+
+function handleAddTodos(newTodo) {
+  const newTodoList = [...todos, newTodo]
+  setTodos(newTodoList)
+}
+
+function handleDeleteTodo(index) {
+  const newTodoList = todos.filter((todo, todoIndex) => {
+    return todoIndex != index
+  })
+  setTodos(newTodoList)
+}
+
+function handleEditTodo(index){
+  const valueToBeEdited = todos[index]
+  setTodoValue(valueToBeEdited)
+  handleDeleteTodo(Index)
+}
+
+
+  return (
+    <>
+    <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos}/>
+    <TodoList handleDeleteTodo={handleDeleteTodo} handleEditTodo={handleEditTodo} todos={todos}/>
+    </>
+  )
+}
+
+
+
+export default App
